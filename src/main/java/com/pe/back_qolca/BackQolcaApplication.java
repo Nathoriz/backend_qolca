550ea087bfa7f7778f7bc86910776fa78f765bee
Mod <natalyortiz.401@gmail.com>
2021-06-05 11:34:33 -0500

    Creación del proyecto

---
+++ src/main/java/com/pe/back_qolca/BackQolcaApplication.java
@@ -0,0 +1,13 @@
+package com.pe.back_qolca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackQolcaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackQolcaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pe/back_qolca/BackQolcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.back_qolca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackQolcaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
